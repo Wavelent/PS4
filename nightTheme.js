6ab7cf9b9258a6fdfcdbd046a871189f2a985a88
Wavelent <80627564+Wavelent@users.noreply.github.com>
2022-07-02 13:47:25 +0300

    nightTheme

---
+++ nightTheme.js
@@ -0,0 +1,37 @@
+const toggle = document.getElementById("toggle");
+const refresh = document.getElementById("refresh");
+const theme = window.localStorage.getItem("theme");
+const label = document.getElementById("label");
+
+/* checks if the theme stored in localStorage is dark
+if yes apply the dark theme to the body */
+if (theme === "dark") document.body.classList.add("dark");
+
+// event listener stops when the change theme button is clicked
+label.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (theme === "dark") {
+    
+    window.localStorage.setItem("theme", "light");
+  } else window.localStorage.setItem("theme", "dark");
+});
+
+// save checkbox state
+function save() {	
+	var checkbox = document.getElementById("checkbox1");
+    localStorage.setItem("checkbox1", checkbox.checked);	
+}
+
+$("#checkbox-container :checkbox").on("change", function(){
+    alert("The checkbox with the ID '" + this.id + "' changed");
+  });
+// refresh.addEventListener("click", () => {
+//   window.location.reload();
+// });
+// document.getElementById("label").onclick = function () {
+//   if (document.getElementById("switch").checked) {
+//     alert("Чекбокс нажат -вариант №2");
+//   } else {
+//     alert("Чекбокс не нажат-вариант №2");
+//   }
+// };
